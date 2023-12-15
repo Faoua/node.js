ec787052cb512edd704bd26e7ad80e7f514c09ef
Your Name <you@example.com>
2023-12-15 19:40:24 +0100

    changement de app

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
+ // app.js
 const express = require('express');
-const app = express();
 const passport = require('passport');
+const session = require('express-session');
 const LocalStrategy = require('passport-local').Strategy;
-const passportLocalMongoose = require('passport-local-mongoose');
-
-
-
+const User = require('./models/user'); // Le chemin doit être ajusté en fonction de votre structure de projet
+const authRoutes = require('./Routes/authRoutes');
+const securedRoute = require('./Routes/securedRclearoute');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://belmfadwa12:Fadoua2002@cluster0.ajerpsu.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+const app = express();
+const PORT = 3000;
 
+mongoose.connect('mongodb+srv://belmfadwa12:fadoua2002@cluster0.l2t2hbs.mongodb.net/?retryWrites=true&w=majority');
 
+app.use(session({
+  secret: 'votre-secret-de-session',
+  resave: false,
+  saveUninitialized: false
+}));
 
-// Configuration de Passport
-app.use(require('express-session')({
-    secret: 'votre_secret',
-    resave: false,
-    saveUninitialized: false
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
- //ROHEEEEEEEEEE TEKHRAAAAAAAAAAAA 
- 
- ////////rregerger
-
- //regergergerg
-const port = 3000;
-app.listen(port,()=>{
-
-    console.log(' le serveur est bien lancé')
-
-});
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
